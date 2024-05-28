d7966554998c54ed21048557f0273f1642270b61
LUCAS FROTA BARROSO <lfrota@guarany.com.br>
2024-05-28 18:01:09 -0300

    updating repository and view

--- app/microservices/organization_microservice/organization_repository.py
+++ app/microservices/organization_microservice/organization_repository.py
@@ -2,6 +2,7 @@
 from sqlalchemy.orm import Session
 
 from app.microservices.organization_microservice.organization_model import Organization
+from app.microservices.user_microservice.user_model import User
 
 
 class OrganizationRepository:
@@ -9,7 +10,41 @@ class OrganizationRepository:
         self.DB_session = session
 
     def get_all_organizations(self):
-        return self.DB_session.query(Organization).all
+        try:
+            organizations = self.DB_session.query(Organization).all
+        except Exception as e:
+            return str(e), False
+        return organizations, True
+
+
+    def get_users_from_organization(self, organization_id):
+        try:
+            users =(
+                self.DB_session.query(User)
+                .filter(User.organization_id == organization_id)
+                .all()
+            )
+            return users, True
+
+        except Exception as e:
+            return str(e), False
+
+
+
+    def create_organization_db(self, organization_name):
+
+        try:
+            new_organization = Organization(name = organization_name)
+            self.DB_session.add(new_organization)
+            self.DB_session.commit()
+            return new_organization, True
+
+        except Exception as e:
+            self.DB_session.rollback()
+            return str(e), False
+
+
+
 
 
 
--- app/microservices/organization_microservice/organization_view.py
+++ app/microservices/organization_microservice/organization_view.py
@@ -1,14 +1,71 @@
-from fastapi import APIRouter, HTTPException, Depends
+from urllib import request
+
+from fastapi import APIRouter, HTTPException, Depends, Body
 from sqlalchemy.orm import Session
 
 from app.database import get_session
 from app.microservices.organization_microservice.organization_repository import OrganizationRepository
-
+from app.microservices.organization_microservice.organization_schema import OrganizationSchema
 
 router = APIRouter()
 @router.get("/organizations")
 async def get_all_organizations(DB_session : Session = Depends(get_session)):
     repository = OrganizationRepository(DB_session)
-    organizations = repository.get_all_organizations()
+    try:
+     organizations, status = repository.get_all_organizations()
 
-    return organizations
+     if status:
+         return {
+             "status": True,
+             "status_code": 200,
+             "result": organizations,
+         }
+     else:
+         return {
+             "status": False,
+             "status_code": 500,
+             "result": 'failed to get organizations',
+
+         }
+
+
+    except Exception as e:
+
+        return {
+            "status": False,
+            "status_code": 500,
+            "result": str(e),
+
+        }
+@router.post("/organizations", response_model=OrganizationSchema)
+async def create_new_organization(organization_name: str, DB_session: Session = Depends(get_session)):
+    try:
+        repository = OrganizationRepository(DB_session)
+        new_organization, status = repository.create_organization_db(organization_name)
+
+        if status:
+            return {
+                "status": True,
+                "status_code": 201,
+                "result": new_organization,
+            }
+        else:
+            return {
+                "status": False,
+                "status_code": 500,
+                "result": new_organization,
+            }
+
+    except Exception as e:
+        return {
+            "status": False,
+            "status_code": 500,
+            "result": str(e),
+        }
+
+
+
+
+
+
+
